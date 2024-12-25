fb0686cb0f9eeb42db5fd7e789d50b62c2fbf45f
kdara20 <dcjays22@gmail.com>
2024-12-24 22:15:24 -0500

    1-update route files and paths

--- backend/server.js
+++ backend/server.js
@@ -3,9 +3,9 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import { v2 as cloudinary } from "cloudinary";
-import authRoutes from "./routes/auth.routes.js";
-import userRoutes from "./routes/user.routes.js";
-import PostRoutes from "./routes/post.routes.js";
+import authRoutes from "./routes/auth.route.js";
+import userRoutes from "./routes/user.route.js";
+import PostRoutes from "./routes/post.route.js";
 
 import connectMongoDB from "./db/connectMongoDB.js";
 
