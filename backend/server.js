0e63c2836348c7c45129b11bdfb64f5f86319eab
kdara20 <dcjays22@gmail.com>
2024-12-24 20:16:00 -0500

    1-post routes and controllers added

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import cookieParser from "cookie-parser";
 import { v2 as cloudinary } from "cloudinary";
 import authRoutes from "./routes/auth.routes.js";
 import userRoutes from "./routes/user.routes.js";
+import PostRoutes from "./routes/post.routes.js";
 
 import connectMongoDB from "./db/connectMongoDB.js";
 
@@ -27,6 +28,7 @@ app.use(cookieParser()); //to parse the cookie
 
 app.use("/api/auth", authRoutes); //middleware
 app.use("/api/users", userRoutes); //middleware
+app.use("/api/posts", PostRoutes); //middleware
 
 //1st part using get method
 // app.get("/", (req, res) => {
