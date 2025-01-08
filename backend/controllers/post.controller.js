6d4df82f3dbc715c470db2a5ec26b2485c194a3a
kdara20 <dcjays22@gmail.com>
2025-01-08 10:17:38 -0500

    3-like post mutation added

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -85,42 +85,46 @@ export const commentOnPost = async (req, res) => {
 };
 
 export const likeUnlikePost = async (req, res) => {
-    try {
-        const userId = req.user._id;
-        const { id: postId } = req.params;
+	try {
+		const userId = req.user._id;
+		const { id: postId } = req.params;
 
-        const post = await Post.findById(postId);
-        if (!post) {
-            return res.status(404).json({ error: "Post not found" });
-        }
-        const userlikePost = post.likes.includes(userId);
-        if (userlikePost) {
-            // Ulike post
-            await Post.updateOne({ _id: postId }, { $pull: { likes: userId } });
-            res.status(200).json({ message: "Post unliked successfully" });
-            await Post.updateOne({ _id: userId }, { $pull: { likedPosts: postId } });
-            res.status(200).json({ message: "Post unliked successfully" });
+		const post = await Post.findById(postId);
 
-        } else {
-            // Like post
-            post.likes.push(userId);
-            await User.updateOne({ _id: userId }, { $push: { likedPosts: postId } });
-            await post.save();
+		if (!post) {
+			return res.status(404).json({ error: "Post not found" });
+		}
 
-            const notification = new Notification({
-                from: userId,
-                to: post.user,
-                type: "like",
-            });
-            await notification.save();
-            res.status(200).json({ message: "Post liked successfully" });
-        }
-    } catch (error) {
-        console.log("Error in likeUnlikePost controller: ", error);
-        res.status(500).json({ error: "Internal server error" });
+		const userLikedPost = post.likes.includes(userId);
 
-    }
-}
+		if (userLikedPost) {
+			// Unlike post
+			await Post.updateOne({ _id: postId }, { $pull: { likes: userId } });
+			await User.updateOne({ _id: userId }, { $pull: { likedPosts: postId } });
+
+			const updatedLikes = post.likes.filter((id) => id.toString() !== userId.toString());
+			res.status(200).json(updatedLikes);
+		} else {
+			// Like post
+			post.likes.push(userId);
+			await User.updateOne({ _id: userId }, { $push: { likedPosts: postId } });
+			await post.save();
+
+			const notification = new Notification({
+				from: userId,
+				to: post.user,
+				type: "like",
+			});
+			await notification.save();
+
+			const updatedLikes = post.likes;
+			res.status(200).json(updatedLikes);
+		}
+	} catch (error) {
+		console.log("Error in likeUnlikePost controller: ", error);
+		res.status(500).json({ error: "Internal server error" });
+	}
+};
 
 export const getAllPosts = async (req, res) => {
 
--- frontend/src/components/common/Post.jsx
+++ frontend/src/components/common/Post.jsx
@@ -7,6 +7,7 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { toast } from "react-hot-toast";
+
 import LoadingSpinnner from "../common/LoadingSpinner";
 
 // Take post as prop 
@@ -15,7 +16,15 @@ const Post = ({ post }) => {
     const {data: authUser} = useQuery({queryKey: ['authUser']});
     const queryClient = useQueryClient();
 
-    const {mutate:deletePost, isPending} = useMutation({
+    const postOwner = post.user;
+    // // const isLiked = false;
+    const isLiked = post.likes.includes(authUser._id); //array of user ids
+
+    // // Hardcoded value for now
+    // // const isMyPost = true;
+    const isMyPost = authUser._id === post.user._id;
+
+    const {mutate:deletePost, isPending, isDeleting} = useMutation({
         mutationFn: async () => {
             try {
               const res = await fetch(`/api/posts/${post._id}`, 
@@ -36,14 +45,39 @@ const Post = ({ post }) => {
         }
     }) 
 
-            
+    const { mutate: likePost, isPending: isLiking } = useMutation({
+		mutationFn: async () => {
+			try {
+				const res = await fetch(`/api/posts/like/${post._id}`, {
+					method: "POST",
+				});
+				const data = await res.json();
+				if (!res.ok) {
+					throw new Error(data.error || "Something went wrong");
+				}
+				return data;
+			} catch (error) {
+				throw new Error(error);
+			}
+		},
+		onSuccess: (updatedLikes) => {
+			// this is not the best UX, bc it will refetch all posts
+			// queryClient.invalidateQueries({ queryKey: ["posts"] });
 
-    const postOwner = post.user;
-    const isLiked = false;
-
-    // Hardcoded value for now
-    // const isMyPost = true;
-    const isMyPost = authUser._id === post.user._id;
+			// instead, update the cache directly for that post
+			queryClient.setQueryData(["posts"], (oldData) => {
+				return oldData.map((p) => {
+					if (p._id === post._id) {
+						return { ...p, likes: updatedLikes };
+					}
+					return p;
+				});
+			});
+		},
+		onError: (error) => {
+			toast.error(error.message);
+		},
+	});
 
     const formattedDate = "1h";
 
@@ -57,7 +91,11 @@ const Post = ({ post }) => {
         e.preventDefault();
     };
 
-    const handleLikePost = () => { };
+    const handleLikePost = () => {
+        if(isLiking) return;
+        likePost();
+    };
+
 
     return (
         <>
@@ -79,9 +117,10 @@ const Post = ({ post }) => {
                         </span>
                         {isMyPost && (
                             <span className='flex justify-end flex-1'>
-                               {!isPending &&<FaTrash className='cursor-pointer hover:text-red-500' onClick={handleDeletePost} />}
+                               {!isDeleting && (<FaTrash className='cursor-pointer hover:text-red-500' onClick={handleDeletePost} />
+                            )}
 
-                                {isPending && (<LoadingSpinnner size='sm' /> )}
+                                {isDeleting && <LoadingSpinnner size='sm' /> }
                             </span>
                         )}
                     </div>
@@ -148,11 +187,9 @@ const Post = ({ post }) => {
                                             onChange={(e) => setComment(e.target.value)}
                                         />
                                         <button className='btn btn-primary rounded-full btn-sm text-white px-4'>
-                                            {isCommenting ? (
-                                                <span className='loading loading-spinner loading-md'></span>
-                                            ) : (
-                                                "Post"
-                                            )}
+                                            {isCommenting ? 
+                                                // <span className='loading loading-spinner loading-md'></span>
+                                                <LoadingSpinnner size='md' /> : "Post"}
                                         </button>
                                     </form>
                                 </div>
@@ -165,13 +202,14 @@ const Post = ({ post }) => {
                                 <span className='text-sm text-slate-500 group-hover:text-green-500'>0</span>
                             </div>
                             <div className='flex gap-1 items-center group cursor-pointer' onClick={handleLikePost}>
-                                {!isLiked && (
+                                {isLiking && <LoadingSpinnner size='sm' />}
+                                {!isLiked && !isLiking && (
                                     <FaRegHeart className='w-4 h-4 cursor-pointer text-slate-500 group-hover:text-pink-500' />
                                 )}
-                                {isLiked && <FaRegHeart className='w-4 h-4 cursor-pointer text-pink-500 ' />}
+                                {isLiked && !isLiking && <FaRegHeart className='w-4 h-4 cursor-pointer text-pink-500 ' />}
 
                                 <span
-                                    className={`text-sm text-slate-500 group-hover:text-pink-500 ${isLiked ? "text-pink-500" : ""
+                                    className={`text-sm group-hover:text-pink-500 ${isLiked ? "text-pink-500" : "text-slate-500"
                                         }`}
                                 >
                                     {post.likes.length}
