710afaa3f6775e561a3e90240ce4c96301665bf2
kdara20 <dcjays22@gmail.com>
2025-01-10 08:10:21 -0500

    3-Profile page posts and user info fetched

--- frontend/src/components/common/Posts.jsx
+++ frontend/src/components/common/Posts.jsx
@@ -4,58 +4,73 @@ import PostSkeleton from "../skeletons/PostSkeleton.jsx";
 // import { POSTS } from "../../utils/db/dummy";
 import { useQuery } from "@tanstack/react-query";
 
-const Posts = ({feedType}) => {
-    // const isLoading = false;
+const Posts = ({ feedType, username, userId }) => {
+	// const isLoading = false;
 
-    const getPostEndpoint = () => {
-        switch (feedType) {
-            case 'forYou':
-                return '/api/posts/all';
-            case 'following':
-                return '/api/posts/following';
-            default:
-                return '/api/posts/all';
-        }
-    }
+	// There are 3 endpoints for posts
+	const getPostEndpoint = () => {
+		switch (feedType) {
+			case "forYou":
+				return "/api/posts/all";
+			case "following":
+				return "/api/posts/following";
+			case "posts":
+				return `/api/posts/user/${username}`;
+			case "likes":
+				return `/api/posts/likes/${userId}`;
+			default:
+				return "/api/posts/all";
+		}
+	};
 
-    const POST_ENDPOINT = getPostEndpoint();
+	const POST_ENDPOINT = getPostEndpoint();
 
-    const {data: posts, isLoading, refetch, isRefetching} = useQuery({queryKey:['posts'], queryFn: async () => {try {
-        const res = await fetch(POST_ENDPOINT);
-        const data = await res.json();
-        if (!res.ok) {
-            throw new Error(data.error || 'Something went wrong!');
-        }
-        return data;
+	const {
+		data: posts,
+		isLoading,
+		refetch,
+		isRefetching,
+	} = useQuery({
+		queryKey: ["posts"],
+		queryFn: async () => {
+			try {
+				const res = await fetch(POST_ENDPOINT);
+				const data = await res.json();
+				if (!res.ok) {
+					throw new Error(data.error || "Something went wrong!");
+				}
+				return data;
+			} catch (error) {
+				throw new Error(error);
+			}
+		},
+	});
 
-    } catch (error) {
-        throw new Error(error);
-    }}
-});
+	// refetch the data when feedType changes in the following
+	useEffect(() => {
+		refetch();
+	}, [feedType, refetch]);
 
-// refetch the data when feedType changes in the following 
-useEffect(() =>{
-    refetch();
-},[feedType, refetch]);
-
-    return (
-        <>
-            {(isLoading || isRefetching) && (
-                <div className='flex flex-col justify-center'>
-                    <PostSkeleton />
-                    <PostSkeleton />
-                    <PostSkeleton />
-                </div>
-            )}
-            {!isLoading && !isRefetching && posts?.length === 0 && <p className='text-center my-4'>No posts in this tab. Switch 👻</p>}
-            {!isLoading && !isRefetching && posts && (
-                <div>
-                    {posts.map((post) => (
-                        <Post key={post._id} post={post} />
-                    ))}
-                </div>
-            )}
-        </>
-    );
+	return (
+		<>
+			{(isLoading || isRefetching) && (
+				<div className="flex flex-col justify-center">
+					<PostSkeleton />
+					<PostSkeleton />
+					<PostSkeleton />
+				</div>
+			)}
+			{!isLoading && !isRefetching && posts?.length === 0 && (
+				<p className="text-center my-4">No posts in this tab. Switch 👻</p>
+			)}
+			{!isLoading && !isRefetching && posts && (
+				<div>
+					{posts.map((post) => (
+						<Post key={post._id} post={post} />
+					))}
+				</div>
+			)}
+		</>
+	);
 };
 export default Posts;
